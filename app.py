7eaa11b521b7b8303aa7b57e110f270f85a930a5
Sidhue96 <musician.sidhue96@gmail.com>
2021-08-24 20:12:44 +0530

    scheduler not possible without oauth token

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import json
 import pytz
 import pymongo
-import subprocess
 import pandas as pd
 
 from datetime import datetime
@@ -29,11 +28,9 @@ def index():
 
     user_settings = twitter.get("account/settings.json")
     screen_name = user_settings.json()['screen_name']
-    print(screen_name)
     mclient = pymongo.MongoClient(os.environ['MONGO_CLIENT'])
     mdb = mclient[os.environ['DB_NAME']]
     count = mdb[screen_name].count()
-    print(count)
     exceptions = []
     start_date, end_date = None, None
 
@@ -74,7 +71,13 @@ def index():
             '$lte': start_date
         }
     
-    print(to_find)
+    if 'sort' in request.args:
+        order_by = -1 if request.args['sort'] == 'desc' else 1
+        to_find['$query'] = {}
+        to_find['$orderby'] = {
+            'created_at_dt': order_by
+        }
+    
     
     if count == 0:
         resp = twitter.get("statuses/user_timeline.json")
@@ -88,15 +91,48 @@ def index():
         df['created_at_dt'] = pd.to_datetime(df['created_at'])
         _ = mdb[screen_name].insert_many(df.to_dict('records'))
 
-    resp = list(mdb[screen_name].find(to_find, {'_id': 0}))
+    resp = list(mdb[screen_name].find(to_find, {'_id': 0, 'created_at_dt': 0}))
+    
+    if 'view' in request.args and request.args['view'] == 'table':
+        df = pd.DataFrame(resp)
+        return df.to_html()
+        
+    return {
+        'statusCode': 200,
+        'body': resp,
+        'exceptions': exceptions
+    }
 
-    asc = True
-    if 'sort' in request.args and request.args['sort'] == 'desc':
-        asc = False
+@app.route("/sync-tweets", methods=["GET"])
+@cross_origin()
+def sync_tweets():
+    if not twitter.authorized:
+        return redirect(url_for("twitter.login"))
+    
+    user_settings = twitter.get("account/settings.json")
+    screen_name = user_settings.json()['screen_name']
+    mclient = pymongo.MongoClient(os.environ['MONGO_CLIENT'])
+    mdb = mclient[os.environ['DB_NAME']]
+    count = mdb[screen_name].count()
 
-    df = pd.DataFrame(resp)
-    df.sort_values(by='created_at_dt', inplace=True, ascending=asc)
-    # todo convert as api with exceptions
+    if count == 0:
+        resp = twitter.get("statuses/user_timeline.json")
+    else:
+        record = mdb[screen_name].find_one({}, sort=[('created_at_dt', pymongo.DESCENDING)])
+        resp = twitter.get("statuses/user_timeline.json?since_id={since_id}".format(since_id=record['id']))
+    if not resp.ok:
+        return {
+            'statusCode': 500,
+            'body': 'Internal server error'
+        }
+    resp = resp.json()
+    if len(resp) > 0:
+        df = pd.DataFrame(resp)
+        df['created_at_dt'] = pd.to_datetime(df['created_at'])
+        _ = mdb[screen_name].insert_many(df.to_dict('records'))
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(True)
+    }
 
-    # return df.drop('created_at_dt', axis=1).to_dict('records')
-    return df.to_html()
