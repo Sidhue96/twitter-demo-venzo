097e89d8c30eec966540485aab828648e81e0807
Sidhue96 <musician.sidhue96@gmail.com>
2021-08-24 19:10:43 +0530

    sorting & filtering is done

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import json
+import pytz
 import pymongo
 import subprocess
 import pandas as pd
 
-from flask import Flask, redirect, url_for
+from datetime import datetime
+from flask import Flask, redirect, url_for, request
 from flask_cors import CORS, cross_origin
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 
@@ -27,16 +29,54 @@ def index():
 
     user_settings = twitter.get("account/settings.json")
     screen_name = user_settings.json()['screen_name']
-
+    print(screen_name)
     mclient = pymongo.MongoClient(os.environ['MONGO_CLIENT'])
     mdb = mclient[os.environ['DB_NAME']]
     count = mdb[screen_name].count()
+    print(count)
+    exceptions = []
+    start_date, end_date = None, None
+
+    if 'start' in request.args:
+        try:
+            start_date = datetime.strptime(request.args['start'], "%Y-%m-%d %H:%M:%S")
+            start_date = pytz.utc.localize(start_date)
+
+        except Exception as e:
+            exceptions.append({
+                'exception': str(e),
+                'body': 'unable to filter based on start date'
+            })
     
-    if count > 0:
-        print('fetching from db')
-        resp = list(mdb[screen_name].find({}, {'_id': 0}))
-    else:
-        print('fetching from api')
+    if 'end' in request.args:
+        try:
+            end_date = datetime.strptime(request.args['end'], "%Y-%m-%d %H:%M:%S")
+            end_date = pytz.utc.localize(end_date)
+
+        except Exception as e:
+            exceptions.append({
+                'exception': str(e),
+                'body': 'unable to filter based on end date'
+            })
+        
+    to_find = {}
+    if start_date is not None and end_date is not None:
+        to_find['created_at_dt'] = {
+            '$gte': start_date,
+            '$lte': end_date
+        }
+    elif start_date is not None:
+        to_find['created_at_dt'] = {
+            '$gte': start_date
+        }
+    elif end_date is not None:
+        to_find['created_at_dt'] = {
+            '$lte': start_date
+        }
+    
+    print(to_find)
+    
+    if count == 0:
         resp = twitter.get("statuses/user_timeline.json")
         if not resp.ok:
             return {
@@ -44,10 +84,19 @@ def index():
                 'body': 'Internal server error'
             }
         resp = resp.json()
-        _ = mdb[screen_name].insert_many(resp)
+        df = pd.DataFrame(resp)
+        df['created_at_dt'] = pd.to_datetime(df['created_at'])
+        _ = mdb[screen_name].insert_many(df.to_dict('records'))
+
+    resp = list(mdb[screen_name].find(to_find, {'_id': 0}))
+
+    asc = True
+    if 'sort' in request.args and request.args['sort'] == 'desc':
+        asc = False
 
     df = pd.DataFrame(resp)
-    df['created_at_dt'] = pd.to_datetime(df['created_at'])
-    df.sort_values(by='created_at_dt', inplace=True)
+    df.sort_values(by='created_at_dt', inplace=True, ascending=asc)
+    # todo convert as api with exceptions
 
+    # return df.drop('created_at_dt', axis=1).to_dict('records')
     return df.to_html()
