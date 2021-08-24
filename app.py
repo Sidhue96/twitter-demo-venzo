5142c45bec78a4bde909a435469edc2b0ff8a7ce
Sidhue96 <musician.sidhue96@gmail.com>
2021-08-24 18:29:19 +0530

    chronological order done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 import pymongo
 import subprocess
+import pandas as pd
 
 from flask import Flask, redirect, url_for
 from flask_cors import CORS, cross_origin
@@ -23,7 +24,30 @@ app.register_blueprint(blueprint, url_prefix="/login")
 def index():
     if not twitter.authorized:
         return redirect(url_for("twitter.login"))
-    resp = twitter.get("statuses/user_timeline.json")
-    assert resp.ok
 
-    return json.dumps(resp.json())
+    user_settings = twitter.get("account/settings.json")
+    screen_name = user_settings.json()['screen_name']
+
+    mclient = pymongo.MongoClient(os.environ['MONGO_CLIENT'])
+    mdb = mclient[os.environ['DB_NAME']]
+    count = mdb[screen_name].count()
+    
+    if count > 0:
+        print('fetching from db')
+        resp = list(mdb[screen_name].find({}, {'_id': 0}))
+    else:
+        print('fetching from api')
+        resp = twitter.get("statuses/user_timeline.json")
+        if not resp.ok:
+            return {
+                'statusCode': 500,
+                'body': 'Internal server error'
+            }
+        resp = resp.json()
+        _ = mdb[screen_name].insert_many(resp)
+
+    df = pd.DataFrame(resp)
+    df['created_at_dt'] = pd.to_datetime(df['created_at'])
+    df.sort_values(by='created_at_dt', inplace=True)
+
+    return df.to_html()
